6013fd4895768c8655471506dfd9a2b75f1446b4
ohslyfox <patricksfinger@gmail.com>
2020-09-17 00:39:54 -0700

    scene transition and task trigger base

---
+++ Assets/Scripts/SampleTaskTrigger.cs
@@ -0,0 +1,41 @@
+﻿using Scenes;
+using UnityEngine;
+
+namespace TaskTriggers
+{
+    public class SampleTaskTrigger : MonoBehaviour
+    {
+        private TaskTriggerBase TaskTrigger { get; set; }
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.TaskTrigger = new CollideWithBoxTrigger();
+            SampleSceneTrigger.SampleSceneTransitionSingleton.TaskList.Add(this.TaskTrigger);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            this.TaskTrigger.Interact();
+        }
+
+        private class CollideWithBoxTrigger : TaskTriggerBase
+        {
+            public CollideWithBoxTrigger()
+                : base () 
+            { }
+
+            public override void Interact()
+            {
+                this.RequirementsMet = true;
+            }
+        }
+    }
+}
+
