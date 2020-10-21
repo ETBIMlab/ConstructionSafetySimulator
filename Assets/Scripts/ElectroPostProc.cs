1323cccb2e84e2aeadf17ed9c9c91e9e5b3c4d91
Rustin Richardson <rustin.richardson@aps.com>
2020-10-20 19:15:01 -0700

    ConstructionScene: Initial Commit
    Several changes were implemented:
    -Upgraded to Unity's URP render pipeline
      -this gives access to shader graph
      -remapped the majority of the materials to use the new pipeline
      -created new shader to replace the old interactable object one
    -Created new scene for contruction site
      -added terrains to the scene
        -created custom triplanar shader and material for ditches
        -this allows the textures to transition smoothly throughout terrain
      -modeled trench around what was provided from SketchUp
      -added crane, pipe, and human to scene for scale
      -did example terrain modification to test terrain tools
      -tested system on Oculus Quest using OpenVR (SteamVR)
    -Added terrain assets to the project

--- Assets/Scripts/ElectroPostProc.cs
+++ Assets/Scripts/ElectroPostProc.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+//using UnityEngine.Rendering.PostProcessing;
 
 //AUTHOR: William Jenkins
 //Trigger script to effect targeted post processing profile
@@ -11,13 +11,13 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class ElectroPostProc : MonoBehaviour
 {
-    public PostProcessVolume targetProfile;
+    //public PostProcessVolume targetProfile;
     public float vignetteAmount = 1.0f;
     public float saturationAmount = -100.0f;
     public float contrastAmount = 60.0f;
     public float exposureAmount = 2.0f;
-    private ColorGrading _colorgrading;
-    private Vignette _vignette;
+    //private ColorGrading _colorgrading;
+    //private Vignette _vignette;
     private float currentContrast;
     private float currentSaturation;
     private float currentVignette;
@@ -26,12 +26,12 @@ public class ElectroPostProc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        targetProfile.profile.TryGetSettings(out _colorgrading);
+       /* targetProfile.profile.TryGetSettings(out _colorgrading);
         targetProfile.profile.TryGetSettings(out _vignette);
         currentContrast = _colorgrading.contrast.value;
         currentSaturation = _colorgrading.saturation.value;
         currentVignette = _vignette.intensity.value;
-        currentExposure = _colorgrading.postExposure.value;
+        currentExposure = _colorgrading.postExposure.value;*/
     }
 
     // Update is called once per frame
@@ -41,17 +41,17 @@ public class ElectroPostProc : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-        _colorgrading.saturation.value = saturationAmount;
+        /*_colorgrading.saturation.value = saturationAmount;
         _colorgrading.contrast.value = contrastAmount;
         _vignette.intensity.value = vignetteAmount;
-        _colorgrading.postExposure.value = exposureAmount;
+        _colorgrading.postExposure.value = exposureAmount;*/
     }
 
     private void OnTriggerExit(Collider other) {
-        _colorgrading.saturation.value = currentSaturation;
+        /*_colorgrading.saturation.value = currentSaturation;
         _colorgrading.contrast.value = currentContrast;
         _vignette.intensity.value = currentVignette;
         _colorgrading.postExposure.value = currentExposure;
-
+*/
     }
 }
