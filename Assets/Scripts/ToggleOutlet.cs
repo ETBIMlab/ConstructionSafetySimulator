1bb6963a70a86c6cea8114839cd72486e37bcc19
William Jenkins <jenkinswilliam89@gmail.com>
2020-10-29 18:22:09 -0700

    Added script to breaker panel switch1
    - enables/disables a gameObject on contact with hand
    - added a test outlet as proof of concept
    - suggest we add triggered scripts to an empty game object
    - the hand will always fire an event so the current outlet can't be
    disabled

---
+++ Assets/Scripts/ToggleOutlet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOutlet : MonoBehaviour
+{
+    
+    public GameObject triggerObject;
+    private bool enable;
+
+    void Start() {
+        enable = false;
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        triggerObject.SetActive(enable);
+        enable = !enable;
+    }
+}
