a7fe8ffd46543ec30d998f5928c4a8716b1d7a29
Patrick Finger <30889638+ohslyfox@users.noreply.github.com>
2020-11-24 22:38:52 -0700

    cleaned up scene transition code
    - cleaned up overall object models
    - added drop down to inspector for game scripts for easier scene transition association

---
+++ Assets/Scripts/SceneTransitions/Base/SceneTransitionUtility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneTransitions
+{
+    public enum SceneTransitioner
+    {
+        SceneOneToSceneTwo = 0
+    }
+
+    public static class SceneTransitionerExtensions
+    {
+        private static SceneTransitionSingletons Singletons = new SceneTransitionSingletons();
+
+        public static ISelectScenes GetSceneTransitioner(this SceneTransitioner sceneTransitioner)
+        {
+            ISelectScenes res = null;
+            switch (sceneTransitioner)
+            {
+                case SceneTransitioner.SceneOneToSceneTwo:
+                    res = Singletons.SceneOneToSceneTwo;
+                    break;
+            }
+            return res;
+        }
+    }
+
+    public class SceneTransitionSingletons
+    {
+        private ISelectScenes sceneOneToSceneTwo = null;
+        public ISelectScenes SceneOneToSceneTwo
+        {
+            get
+            {
+                if (sceneOneToSceneTwo == null)
+                {
+                    sceneOneToSceneTwo = new DefaultSceneTransition("SampleScene2");
+                }
+                return sceneOneToSceneTwo;
+            }
+        }
+    }
+}
