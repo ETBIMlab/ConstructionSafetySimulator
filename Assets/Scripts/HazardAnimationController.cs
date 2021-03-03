4c94cd6729ed67695466ea48a145887fc1c6d868
Adam Hardy <ahardy313@gmail.com>
2021-03-02 18:45:39 -0700

    Created script for animation control

---
+++ Assets/Scripts/HazardAnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardAnimationController : MonoBehaviour
+{
+    [Tooltip("The object with an animator attached to it")]
+    [SerializeField] public Animator animationController;
+    [Tooltip("The keyboard key that will start the animation.")]
+    [SerializeField] public KeyCode keycode;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(keycode)) {
+            animationController.SetBool("KeyPressed", true);
+        }
+    }
+}
