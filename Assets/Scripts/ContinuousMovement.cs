01274db5073d2237118141fad03ff7a24b53816c
Nevin Foster <nafoste2@asu.edu>
2021-04-01 02:44:42 -0700

    Added some animations to the scene.

---
+++ Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(CharacterController))]
+public class ContinuousMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public float gravity = -9.81f;
+    public SteamVR_Action_Vector2 inputSource;
+    public LayerMask groundLayer;
+    public float additionalHeight;
+    public Camera SteamVRCamera;
+
+    private float fallingSpeed;
+    private Vector2 inputAxis;
+    private CharacterController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+
+        Quaternion rotation = Quaternion.Euler(0, SteamVRCamera.transform.eulerAngles.y, 0);
+        Vector3 direction = rotation * new Vector3(inputAxis.x, 0, inputAxis.y);
+        character.Move(direction * Time.fixedDeltaTime * speed);
+         
+        if (CheckGrounded())
+            fallingSpeed = 0;
+        else
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+
+
+    }
+    
+
+    void CapsuleFollowHeadset()
+    {
+        /*
+        Vector3 distanceMoved = transform.InverseTransformPoint(rig.cameraGameObject.transform.position) - character.center;
+        distanceMoved.Scale(new Vector3(1, 0, 1));
+        rig.transform.position -= distanceMoved;
+        character.Move(distanceMoved);*/
+
+        //character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        //Vector3 campos = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        //character.center = new Vector3(campos.x, character.height / 2 + character.skinWidth, campos.z);
+    }
+
+    bool CheckGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLenth = character.center.y + 0.003f;
+        return Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLenth, groundLayer);
+    }
+}
--- Assets/Scripts/EditorFunctions.cs
+++ Assets/Scripts/EditorFunctions.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine.Animations;
 
 public class EditModeFunctions : EditorWindow
 {
-    GameObject source;
+    GameObject go;
+    float arg_float;
+    int arg_int;
+
+    
     [MenuItem("Window/Edit Mode Functions")]
     public static void ShowWindow()
     {
@@ -13,29 +18,100 @@ public class EditModeFunctions : EditorWindow
     private void OnGUI()
     {
         EditorGUILayout.BeginHorizontal();
-        source = (GameObject) EditorGUILayout.ObjectField(source, typeof(GameObject), true);
+        GUILayout.Label("go ");
+        go = (GameObject) EditorGUILayout.ObjectField(go, typeof(GameObject), true);
         EditorGUILayout.EndHorizontal();
-        if (GUILayout.Button("Shrink Colliders"))
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("arg_float ");
+        arg_float = EditorGUILayout.FloatField(arg_float);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("arg_int ");
+        arg_int = EditorGUILayout.IntField(arg_int);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+        GUILayout.Label("ScaleColliders( go , arg_float ); Scales all colliders that are on any children of this object.");
+        if (GUILayout.Button("Scale Colliders"))
         {
-            ShinkColliders(source.transform);
+            ScaleColliders(go.transform, arg_float);
+        }
+
+        EditorGUILayout.Space();
+        GUILayout.Label("CreateBasicRope( go , arg_int ); Creates a rope with n sections using 'go' as the particle prefab.");
+        if (GUILayout.Button("Create Basic Rope"))
+        {
+            CreateBasicRope(go, arg_int);
         }
     }
 
-    private void ShinkColliders(Transform trans)
+    private void ScaleColliders(Transform trans, float scale)
     {
         Debug.Log("The function ran on: " + trans.name);
         BoxCollider box = null;
         CapsuleCollider cap = null;
         if((box = trans.GetComponent<BoxCollider>()) != null)
         {
-            box.size *=  0.94f;
+            box.size *= scale;
         } else if ((cap = trans.GetComponent<CapsuleCollider>()) != null)
         {
-            cap.radius *= 0.94f;
-            cap.height *= 0.94f;
+            cap.radius *= scale;
+            cap.height *= scale;
         }
 
         foreach (Transform t in trans)
-            ShinkColliders(t);
+            ScaleColliders(t, scale);
+    }
+    
+    private void CreateBasicRope(GameObject prefab, int sections)
+    {
+        if(sections < 2 || sections > 100)
+        {
+            Debug.Log("Usage: sections must be less than 100 but greater than 2");
+        }
+        GameObject rope = new GameObject();
+        rope.transform.position = Vector3.zero;
+        rope.name = "BasicRope";
+
+        GameObject temp;
+        temp = Instantiate<GameObject>(prefab, Vector3.zero, Quaternion.Euler(0,0,0), rope.transform);
+        temp.name = "start";
+        
+        for (int i = 0; i < sections - 2; i++)
+        {
+
+            temp = Instantiate<GameObject>(prefab, Vector3.zero, Quaternion.Euler(0, 0, 0), rope.transform);
+            temp.name = "mid (" + i + ")";
+        }
+
+        temp = Instantiate<GameObject>(prefab, Vector3.zero, Quaternion.Euler(0, 0, 0), rope.transform);
+        temp.name = "end";
+
+        for(int i = 1; i < sections - 1; i++)
+        {
+            ParentConstraint pc = rope.transform.GetChild(i).gameObject.AddComponent<ParentConstraint>();
+            //int connectDistance = (int) Mathf.Floor(Mathf.Log((    ((i % (sections / 2)) != i) ? sections - i : i)    + 1, 2));
+            
+
+            if (pc)
+            {
+                pc.AddSource(new ConstraintSource
+                {
+                    sourceTransform = rope.transform.GetChild(0),
+                    weight = 1 - (((float) i) / ((float)(sections - 1)))
+            });
+                pc.AddSource(new ConstraintSource
+                {
+                    sourceTransform = rope.transform.GetChild(sections - 1),
+                    weight = (((float)i) / ((float)(sections - 1)))
+                });
+                pc.constraintActive = true;
+                pc.locked = true;
+            }
+        }
+        
+
     }
 }
