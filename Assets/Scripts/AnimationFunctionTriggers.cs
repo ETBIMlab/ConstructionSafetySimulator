1d28a9d95df169ca352fb6083126dabe3dd55e0e
Nevin Foster <nafoste2@asu.edu>
2021-07-15 20:55:20 -0700

    Added Shack swaying and Hazard 2 haptics

--- Assets/Scripts/AnimationFunctionTriggers.cs
+++ Assets/Scripts/AnimationFunctionTriggers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Animator))]
 public class AnimationFunctionTriggers : MonoBehaviour
 {
     [SerializeField]
