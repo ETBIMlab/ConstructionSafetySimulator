e2fb984f3af6ede264f1f61973bc77063f30ab8b
Austin Porter <aportercoder@gmail.com>
2022-02-24 20:48:32 -0700

    Reset and Rewind Timeline in test scene
    reset and rewinding done in test scene. Need to try with actual scene.

--- Assets/OnboardingTasks/AustinPorter/Scripts/OnInputPress.cs
+++ Assets/OnboardingTasks/AustinPorter/Scripts/OnInputPress.cs
@@ -5,14 +5,30 @@ using UnityEngine.Events;
 
 public class OnInputPress : MonoBehaviour
 {
-    public List<UnityEvent> spaceKey;
+    public List<UnityEvent> resetTimelineKey;
+    public List<UnityEvent> toggleTimelineStateKey;
+    public List<UnityEvent> toggleTimelineFreezeKey;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("space"))
         {
-            foreach(UnityEvent e in spaceKey) {
+            foreach(UnityEvent e in resetTimelineKey) {
+                e.Invoke(); //call the current unity event
+            }
+        }
+        if (Input.GetKeyDown("f"))
+        {
+            foreach (UnityEvent e in toggleTimelineFreezeKey)
+            {
+                e.Invoke(); //call the current unity event
+            }
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            foreach (UnityEvent e in toggleTimelineStateKey)
+            {
                 e.Invoke(); //call the current unity event
             }
         }
