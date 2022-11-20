fbe07b568e61cf9d9827c18ee22fbd33cb765645
EnlightenedPanda <perrywise31@gmail.com>
2022-11-20 13:56:46 -0700

    Hazard 2 Rough Draft
    Aligned my Hazard 2 timeline with the Stringing Scene and put everything into my test scene named "Hazard2NP" in the NPAssets folder

---
+++ Assets/NP Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement: MonoBehaviour
+{
+
+    public UnityEngine.CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // rotate character to face directly ahead
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
