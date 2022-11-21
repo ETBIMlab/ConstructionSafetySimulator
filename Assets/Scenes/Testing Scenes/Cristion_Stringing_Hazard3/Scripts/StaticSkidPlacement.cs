a97053d0e74962f4dbeb91907e8bb973b409b706
Creseq <cristiondominguez@gmail.com>
2022-11-20 23:15:20 -0700

    Insert a repeating skid timeline in Stringing scene

---
+++ Assets/Scenes/Testing Scenes/Cristion_Stringing_Hazard3/Scripts/StaticSkidPlacement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class StaticSkidPlacement : MonoBehaviour
+{
+    [SerializeField] PlayableDirector _skidDirector;
+    [SerializeField] Transform _skid;
+    List<Transform> _planks = new List<Transform>();
+    int _plankIndex;
+
+    private void Awake()
+    {
+        for (int i = _skid.childCount - 1; i > 1; i--)
+            _planks.Add(_skid.GetChild(i));
+    }
+
+    private void Start()
+    {
+        PlayTimeline();
+    }
+
+    public void PlayTimeline()
+    {
+        if (_plankIndex >= _planks.Count)
+            return;
+
+        _skidDirector.Play();
+    }
+
+    public void PlacePlank()
+    {
+        _planks[_plankIndex].gameObject.SetActive(true);
+        _plankIndex++;
+    }
+}
