ba2bbdaf6e4d920414fb48a5dc146f3c3dfd10ca
mnfitz-ASU <mnfitzg1@asu.edu>
2023-04-18 19:54:58 -0700

    Git cherrypick 1fb6aaa6

---
+++ Assets/SpeedController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+
+/*
+    animation.Stop();
+    animation.Play("Idle");   
+
+
+    function Update () 
+    {
+        float speed = (transform.position - this.mLastPosition).magnitude / elapsedTime;
+        this.mLastPosition = transform.position;
+        
+        if (INSERT_SPEED_VARIABLE_HERE) > 0.1)
+        {
+            animation.CrossFade("WalkCycle");
+        }
+    }
+*/
+}
